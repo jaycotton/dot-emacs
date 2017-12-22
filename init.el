b3cf9a01f4f3cbc599db1614a9d78f7e8e62f250
Jay Cotton <jay.cotton@gmail.com>
2017-12-22 14:18:09 -0500

    Initial emacs init commit

---
+++ init.el
@@ -0,0 +1,5 @@
+(require 'org)
+(find-file (concat user-emacs-directory "init.org"))
+(org-babel-tangle)
+(load-file (concat user-emacs-directory "init.el"))
+(byte-compile-file (concat user-emacs-directory "init.el"))
